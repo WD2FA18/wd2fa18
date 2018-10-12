f08ab3ab295dbef4dc1335fba9aed9ec1f206cae
Luke@Butte <sathrumlu@butte.edu>
2018-10-12 17:48:59 +0000

    adding express files

---
+++ instructions.js
@@ -0,0 +1,14 @@
+// To install Express
+npm install express
+
+// To install express-generator
+npm install express-generator
+
+// Setup the Web Framework
+./node_modules/.bin/express --view=ejs --git
+
+// Install Dependencies
+npm install
+
+// Run your application
+npm start
