b482fa55dd6a6a99d05613df7ba733b1dbbfa9c8
Luke@Butte <sathrumlu@butte.edu>
2018-12-04 19:50:00 +0000

    Adding end of class changes

--- sequelize.js
+++ sequelize.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-
+// Bring in the categories model
+const categoryModel = require('./models/categories');
+const productsModel = require('./models/products');
 const sequelize = new Sequelize({
   host: 'localhost',
   database: 'guitar_shop',
@@ -14,5 +16,19 @@ const sequelize = new Sequelize({
     idle: 10000
   },
   // To remove deprecated warning
-  operatorsAliases: false
+  operatorsAliases: false,
+  logging: false
 });
+
+const categories = categoryModel(sequelize, Sequelize);
+const products = productsModel(sequelize, Sequelize);
+products.associate(categories);
+sequelize.sync({alter: true})
+.then(() => {
+  console.log('Database & Tables Created Successfully!');
+});
+
+module.exports = {
+  categories,
+  products,
+};
